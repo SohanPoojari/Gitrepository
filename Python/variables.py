f670da53f995d6029e018d7062849d2cb18d9b22
SohanPoojari <119739212+SohanPoojari@users.noreply.github.com>
2024-09-19 20:45:54 +0530

    Add files via upload

---
+++ Python/variables.py
@@ -0,0 +1,25 @@
+a=5
+b=10
+
+#here a and b are variables also sum,multi,div,sub are also variables
+#in python the variable takes the data types automatically  i.e there is no need of defining a data type
+
+sum=a+b
+multi=a*b
+div=a/b
+sub=a-b
+print("the result is")
+print(sum)
+print(sub)
+print(multi)
+print(div)
+#while defining a string you have to just give"" or''
+name="sohan"
+
+print("My name is",name)
+
+#variable value changes every time you define them
+
+name="Poojari"
+
+print("My name is", name)
